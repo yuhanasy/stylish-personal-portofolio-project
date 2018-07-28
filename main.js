7fd8aa5b1bdfedb4ed0dd0b434b14df1817d2d1b
Yuhanas Yulianto <yuhanas.yulianto@gmail.com>
2018-07-28 20:37:54 +0700

    fix menu button on off

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
   var btn = document.getElementById("menu-button");
   var sideMenu = document.getElementById("menu");
+  var anu = document.getElementsByClassName("bun")
   sideMenu.className = "";
 
   btn.addEventListener('click', function() { active() });
   function active() {
     if (sideMenu.className === "active") {
-      return sideMenu.className = "";
+      sideMenu.className = "";
+
+      anu[0].className = "bun";
+      anu[1].className = "bun";
+      anu[2].className = "bun";
     }
     else {
-      return sideMenu.className = "active";
+      sideMenu.className = "active";
+      
+      anu[0].className += " bunTop";
+      anu[1].className += " bunMiddle";
+      anu[2].className += " bunBottom";
     }
   }
 }, false);
